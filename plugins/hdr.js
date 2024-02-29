99d5eadf741fc473240129faf8e160e8e07cc752
Anowar Sk <anowarsk9749@gmail.com>
2024-03-01 00:53:06 +0530

    𝑴𝑰𝑨 𝑴𝑫

--- plugins/hdr.js
+++ plugins/hdr.js
@@ -18,7 +18,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 				let error;
 				try {
 					const This = await processing(img, "dehaze");
-					conn.sendFile(m.chat, This, "", "Nih...", m);
+					conn.sendFile(m.chat, This, "", "𝑫𝑶𝑵𝑬 𝑩𝑬𝑩𝒀..☯︎", m);
 				} catch (er) {
 					error = true;
 				} finally {
